d2a6596dc7289d90f78b46524ffb0374cdc9ad96
Joe Hartley <joejohnhartley@gmail.com>
2023-01-26 20:14:50 +0000

    added index styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+6.times do
+  flat = Flat.new(
+    name: Faker::Hipster.sentence,
+    address: Faker::Address.full_address,
+    description: Faker::Hipster.paragraph,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(2..20)
+  )
+  flat.save!
+end
